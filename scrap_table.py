269d024ce899ba1b529eb73f27d940cbed62c685
joelm2405 <166072910+joelm2405@users.noreply.github.com>
2024-11-22 22:20:19 -0500

    Add files via upload

--- scrap_table.py
+++ scrap_table.py
@@ -4,7 +4,7 @@ import boto3
 import uuid
 
 def lambda_handler(event, context):
-    url = "https://www.sismologia.cl/"
+    url = "https://ds.iris.edu/latin_am/evlist.phtml?limit=20&new=1"
     response = requests.get(url)
     if response.status_code != 200:
         return {
@@ -13,29 +13,37 @@ def lambda_handler(event, context):
         }
 
     soup = BeautifulSoup(response.content, 'html.parser')
-    table = soup.find('table')
+    table = soup.find('table')  # Encuentra la tabla principal
     if not table:
         return {
             'statusCode': 404,
             'body': 'No se encontró la tabla en la página web'
         }
 
+    # Procesar encabezados de la tabla
     headers = [header.text.strip() for header in table.find_all('th')]
-    rows = []
-    for row in table.find_all('tr')[1:]:
-        cells = row.find_all('td')
-        row_data = {headers[i]: cell.text.strip() for i, cell in enumerate(cells)}
-        rows.append(row_data)
 
+    # Procesar filas de la tabla
+    rows = []
+    for row in table.find_all('tr')[1:]:  # Ignorar la fila de encabezado
+        cells = row.find_all('td')
+        if len(cells) == len(headers):  # Asegurarse de que la fila tenga la misma cantidad de celdas que los encabezados
+            row_data = {headers[i]: cells[i].text.strip() for i in range(len(cells))}
+            rows.append(row_data)
+
+    # Conectar a DynamoDB
     dynamodb = boto3.resource('dynamodb')
-    table = dynamodb.Table('TablaSismosChile')
+    table = dynamodb.Table('TablaWebScrapping')
+
+    # Limpiar tabla existente
     scan = table.scan()
     with table.batch_writer() as batch:
         for each in scan['Items']:
             batch.delete_item(Key={'id': each['id']})
 
+    # Insertar nuevas filas en la tabla DynamoDB
     for row in rows:
-        row['id'] = str(uuid.uuid4())
+        row['id'] = str(uuid.uuid4())  # Agregar un ID único para cada fila
         table.put_item(Item=row)
 
     return {
